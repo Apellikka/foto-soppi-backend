f4097eb3da13eda99feeab1b882dcf7fbf81302c
unknown <aleksimpellikka@gmail.com>
2025-06-14 20:17:51 +0300

    Remove redundant code from SecurityConfig

--- src/main/java/com/apellikka/fotosoppi/foto_soppi/security/SecurityConfiguration.java
+++ src/main/java/com/apellikka/fotosoppi/foto_soppi/security/SecurityConfiguration.java
@@ -3,32 +3,18 @@ package com.apellikka.fotosoppi.foto_soppi.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import com.apellikka.fotosoppi.foto_soppi.service.CustomUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 
-    private final CustomUserDetailsService customUserDetailsService;
-
-    public SecurityConfiguration(CustomUserDetailsService customUserDetailsService) {
-        this.customUserDetailsService = customUserDetailsService;
-    }
-
-    @Bean 
-    UserDetailsService userDetailsService() {
-        return customUserDetailsService;
-    }
+    public SecurityConfiguration() {}
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http, CustomAuthenticationEntryPoint authEntryPoint) throws Exception {
@@ -46,15 +32,7 @@ public class SecurityConfiguration {
 
         return http.build();
     }
-
-    @Bean
-    AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(customUserDetailsService);
-        authenticationProvider.setPasswordEncoder(BCryptPasswordEncoder());
-        return authenticationProvider;
-    }
-
+    
     @Bean
     AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
@@ -62,6 +40,6 @@ public class SecurityConfiguration {
 
     @Bean
     BCryptPasswordEncoder BCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
+         return new BCryptPasswordEncoder();
     }
 }
